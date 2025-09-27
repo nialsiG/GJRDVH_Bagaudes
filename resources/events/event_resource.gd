11cbdadfc1ddd0770272964fc2d43ebe2fc9b5d9
Ghislain Thiery <ghislain.thiery@ntymail.com>
2025-09-27 22:02:19 +0200

    event_resource update

--- resources/events/event_resource.gd
+++ resources/events/event_resource.gd
@@ -4,16 +4,22 @@ class_name EventResource
 @export_group("Event")
 @export var name: String
 @export_multiline var description: String
-@export var texture: Texture2D
 @export var available_on_start: bool = true
 @export var district: Enums.DistrictType = Enums.DistrictType.NONE
+@export var year: int = 1830
 
+@export_group("Popup")
+@export var texture: Texture2D
+@export var event_sound: Enums.Sound
+
+@export_group("Choice1")
 @export_multiline var choice_1: String
 @export var choice_1_contentement: float
 @export var choice_1_health: float
 @export var choice_1_time: int
 @export var choice_1_unlockable_events: Array[EventResource]
 
+@export_group("Choice2")
 @export_multiline var choice_2: String
 @export var choice_2_contentement: float
 @export var choice_2_health: float
